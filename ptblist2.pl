4a276aa445d9e3b4b6f60d61743662c5ece09fb3
Scorbie <scorbiclife@gmail.com>
2015-10-18 20:54:14 +0900

    Create ptblist2.pl
    Displays (number of finished patterns) / (total number of patterns).
    Invoked with the same arguments with those of ptb2. Thus can choose # of catalysts to add, transparency etc.

---
+++ ptblist2.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+
+# Get number of lines
+open TEMP, $ARGV[0];
+1 while(<TEMP>);
+my $all = $.;
+close TEMP;
+
+# Main loop of ptb2
+open PATLIST, $ARGV[0];
+while (<PATLIST>)
+{
+    @flds = split(" ");
+
+    if(scalar(@flds) < 2){
+        $gen0 = 18;
+    }else{
+        $gen0 = $flds[1];
+    }
+   
+    if(scalar(@flds)< 4){
+        $gen1 = $ARGV[2];
+    }elsif($ARGV[2] < $flds[4]){
+        $gen1 = $ARGV[2];
+    }else{
+        $gen1 = $flds[4];
+    }
+   
+    $pat = $flds[0];
+    $pat =~ s/!/.\n/g;
+
+    open OUTPAT, "> temp";
+    print OUTPAT $pat;
+    close OUTPAT;
+
+    print STDERR "==========$./$all==========\n";
+    system("ptb2 temp $ARGV[1] $gen1 $ARGV[3] $gen0 $ARGV[5]");
+
+}
+close PATLIST;
+unlink "temp"
